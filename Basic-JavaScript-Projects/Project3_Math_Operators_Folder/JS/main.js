26a9bdd2a0e005f234846e236f4671d74741e7ef
caduque0906 <caduque0906@gmail.com>
2023-03-31 00:11:01 -0400

    Operator Assignment

--- Basic-JavaScript-Projects/Project3_Math_Operators_Folder/JS/main.js
+++ Basic-JavaScript-Projects/Project3_Math_Operators_Folder/JS/main.js
@@ -1,4 +1,32 @@
-function Math_Function() {
-    var x = 2 + 2;
-    document.getElementById("Math").innerHTML = x;
+function Add() { //adding function Add to button
+    var Addition = 5 + 5; //variable for Add operator
+    document.getElementById("Math_Add").innerHTML = Addition; //displaying addition on p #Math_Add
+}
+function Minus() {
+    var Subtract = 10 - 5; 
+    document.getElementById("Math_Subtract").innerHTML = Subtract;
+}
+function Multiply() {
+    var Multiplication = 5 * 5;
+    document.getElementById("Math_Multiply").innerHTML = Multiplication;
+}
+function Random_Math() {
+    var Random_Num = Math.random();
+    document.getElementById("Math_Random").innerHTML = Random_Num;
+}
+function Mod() {
+    var Modulus = 26 % 7;
+    document.getElementById("Math_Modulus").innerHTML = Modulus;
+}
+function Increase() {
+    var Increment = 5;
+    Increment++
+    var x = Increment;
+    document.getElementById("Math_Increase").innerHTML = Increment;
+}
+function Decrease() {
+    var Decrement = 5;
+    Decrement--
+    var x = Decrement;
+    document.getElementById("Math_Decrease").innerHTML = Decrement;
 }
