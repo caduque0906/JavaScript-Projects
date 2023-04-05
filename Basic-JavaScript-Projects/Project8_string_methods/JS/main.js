647f59767e0d3296f26b8997fbf1b41774f5e99e
caduque0906 <caduque0906@gmail.com>
2023-04-04 20:38:04 -0400

    Updated concat() method

--- Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
+++ Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
@@ -2,9 +2,10 @@ var part_1 = "I really ";
 var part_2 = "the Legend ";
 var part_3 = "of Zelda ";
 var part_4 = "series!";
-var whole_sentence = part_1 + part_2 + part_3 + part_4; //using concat() method
+var whole_sentence = part_1.concat(part_2, part_3, part_4); //using concat() method
 
 var Triforce = "Courage, Wisdom, Power";
+
 var section_slice = Triforce.slice(0,7); //using slice() method on concat method
 var captial_words = whole_sentence.toUpperCase();
 var position = whole_sentence.search("Legend");
