65abdcdc2b187878b729b399b66c700781baaad6
caduque0906 <caduque0906@gmail.com>
2023-04-04 20:23:27 -0400

    Updated project

--- Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
+++ Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
@@ -3,14 +3,16 @@ var part_2 = "the Legend ";
 var part_3 = "of Zelda ";
 var part_4 = "series!";
 var whole_sentence = part_1 + part_2 + part_3 + part_4; //using concat() method
-var section_slice = whole_sentence.slice(13,28); //using slice() method on concat method
+
+var Triforce = "Courage, Wisdom, Power";
+var section_slice = Triforce.slice(0,7); //using slice() method on concat method
 var captial_words = whole_sentence.toUpperCase();
 var position = whole_sentence.search("Legend");
 
 document.getElementById("concat").innerHTML = whole_sentence; //displaying concat method
-document.getElementById("concat").innerHTML = section_slice; //displaying slice method
-document.getElementById("concat").innerHTML = captial_words; //capitalizing all letters
-document.getElementById("concat").innerHTML = position; //using search() method
+document.getElementById("slice").innerHTML = section_slice; //displaying slice method
+document.getElementById("capital").innerHTML = captial_words; //capitalizing all letters
+document.getElementById("search").innerHTML = position; //using search() method
 
 var num = 12.6432;
 var n = num.toFixed(); //rounding up num
